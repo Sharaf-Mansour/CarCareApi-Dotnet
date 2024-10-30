8bacd439f6b6253ad2ff0d42b6d230a3d8b77d92
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2024-10-31 02:21:19 +0300

    Create Car.cs

---
+++ CarCareAPI/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace CarCareAPI.Models;
+public class Car
+{
+    public string Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string ClassId { get; set; }
+    public string LicenseExpirationDate { get; set; }
+    public string ExaminationDate { get; set; }
+    public string NotifyEvery { get; set; }
+    public int Km { get; set; }
+    public string ProfileId { get; set; }
+}
