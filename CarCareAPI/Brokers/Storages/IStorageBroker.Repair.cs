39fc51ad762b513152d229c726f863b1a2c50be5
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-02 10:25:04 +0200

    BROKER:CREATED REPAIR

---
+++ CarCareAPI/Brokers/Storages/IStorageBroker.Repair.cs
@@ -0,0 +1,10 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Brokers.Storages;
+public partial interface IStorageBroker
+{
+    ValueTask InsertRepairAsync(Repair repair);
+    IQueryable<Repair> SelectAllRepairsAsync();
+    ValueTask<Repair> SelectRepairByIdAsync(int repairId);
+    ValueTask UpdateRepairAsync(Repair repair);
+    ValueTask DeleteRepairAsync(int repairId);
+}
---
+++ CarCareAPI/Brokers/Storages/StorageBroker.Repair.cs
@@ -0,0 +1,32 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Brokers.Storages;
+public partial class StorageBroker : IStorageBroker
+{
+    public async ValueTask InsertRepairAsync(Repair repair)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("INSERT INTO Repairs (Id, CarId, Description, Cost, Date) VALUES (@Id, @CarId, @Description, @Cost, @Date);", repair);
+    }
+    public async ValueTask<List<Repair>> SelectAllRepairsAsync()
+    {
+        using var connection = CreateConnection();
+        return (await connection.QueryAsync<Repair>("SELECT * FROM Repairs;")).ToList();
+    }
+    public async ValueTask<Repair> SelectRepairByIdAsync(int repairId)
+    {
+        using var connection = CreateConnection();
+        return await connection.QueryFirstOrDefaultAsync<Repair>("SELECT * FROM Repairs WHERE Id = @Id;", new { Id = repairId });
+    }
+    public async ValueTask UpdateRepairAsync(Repair repair)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("UPDATE Repairs SET CarId = @CarId, Description = @Description, Cost = @Cost, Date = @Date WHERE Id = @Id;", repair);
+    }
+    public async ValueTask DeleteRepairAsync(int repairId)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("DELETE FROM Repairs WHERE Id = @Id;", new { Id = repairId });
+    }
+
+}
+
