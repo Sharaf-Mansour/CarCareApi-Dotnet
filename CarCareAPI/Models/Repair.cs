f425ee8ce8da5622b796e5a82b4f1d83a42bdaa2
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-02 09:10:16 +0200

    DATA:Creaated data models

---
+++ CarCareAPI/Models/Repair.cs
@@ -0,0 +1,10 @@
+﻿namespace CarCareAPI.Models;
+public class Repair
+{
+    public string Id { get; set; }
+    public decimal? Cost { get; set; }
+    public string TypeId { get; set; }
+    public int LastRepairKM { get; set; }
+    public string CarId { get; set; }
+    public string Date { get; set; }
+}
