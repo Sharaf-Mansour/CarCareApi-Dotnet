ac163562e581819ca1b525ae95b790f56f53dd63
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-03 19:38:51 +0200

    Broker:Create ProfileRepairType

---
+++ CarCareAPI/Brokers/Storages/IStorageBroker.ProfileRepairType.cs
@@ -0,0 +1,10 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Brokers.Storages;
+public partial interface IStorageBroker
+{
+    ValueTask InsertProfileRepairTypeAsync(ProfileRepairType profileRepairType);
+    IQueryable<ProfileRepairType> SelectAllProfileRepairTypesAsync();
+    ValueTask<ProfileRepairType> SelectProfileRepairTypeByIdAsync(string profileRepairTypeId);
+    ValueTask UpdateProfileRepairTypeAsync(ProfileRepairType profileRepairType);
+    ValueTask DeleteProfileRepairTypeAsync(string profileRepairTypeId);
+}
---
+++ CarCareAPI/Brokers/Storages/StorageBroker.ProfileRepairType.cs
@@ -0,0 +1,30 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Brokers.Storages;
+public partial class StorageBroker : IStorageBroker
+{
+    public async ValueTask InsertProfileRepairTypeAsync(ProfileRepairType profileRepairType)
+    {
+        using var connection = CreateConnection();
+        await connection.executeAsync("INSERT INTO ProfileRepairType (ProfileId, RepairTypeId) VALUES (@ProfileId, @RepairTypeId);", profileRepairType);
+    }
+    public IQueryable<ProfileRepairType> SelectAllProfileRepairTypesAsync()
+    {
+        using var connection = CreateConnection();
+        return connection.Query<ProfileRepairType>("SELECT * FROM ProfileRepairType;");
+    }
+    public async ValueTask<ProfileRepairType> SelectProfileRepairTypeByIdAsync(string profileRepairTypeId)
+    {
+        using var connection = CreateConnection();
+        return await connection.QueryFirstOrDefaultAsync<ProfileRepairType>("SELECT * FROM ProfileRepairType WHERE Id = @ProfileRepairTypeId;", new { ProfileRepairTypeId = profileRepairTypeId });
+    }
+    public async ValueTask UpdateProfileRepairTypeAsync(ProfileRepairType profileRepairType)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("UPDATE ProfileRepairType SET ProfileId = @ProfileId, RepairTypeId = @RepairTypeId WHERE Id = @Id;", profileRepairType);
+    }
+    public async ValueTask DeleteProfileRepairTypeAsync(string profileRepairTypeId)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("DELETE FROM ProfileRepairType WHERE Id = @ProfileRepairTypeId;", new { ProfileRepairTypeId = profileRepairTypeId });
+    }
+}
