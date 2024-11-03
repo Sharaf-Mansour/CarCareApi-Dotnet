d5af14bd751bab4a49c2b2826bd5831448af081a
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-03 18:55:42 +0200

    Broker:CarClass

---
+++ CarCareAPI/Brokers/Storages/StorageBroker.CarClass.cs
@@ -0,0 +1,30 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Brokers.Storages;
+public partial class StorageBroker : IStorageBroker  
+{
+    public async ValueTask<CarClass> InsertCarClassAsync(CarClass carClass)
+    {
+        using var connection = CreateConnection();
+        await connection.executeAsync("INSERT INTO CarClass (Id, Name) VALUES (@Id, @Name);", carClass);
+    }
+    public async ValueTask<List<CarClass>> SelectAllCarClassesAsync()
+    {
+        using var connection = CreateConnection();
+        return (await connection.QueryAsync<CarClass>("SELECT * FROM CarClass;")).ToList();
+    }
+    public async ValueTask<CarClass> SelectCarClassByIdAsync(string carClassId)
+    {
+        using var connection = CreateConnection();
+        return await connection.QueryFirstOrDefaultAsync<CarClass>("SELECT * FROM CarClass WHERE Id = @Id;", new { Id = carClassId });
+    }
+    public async ValueTask UpdateCarClassAsync(CarClass carClass)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("UPDATE CarClass SET Name = @Name WHERE Id = @Id;", carClass);
+    }
+    public async ValueTask DeleteCarClassAsync(string carClassId)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("DELETE FROM CarClass WHERE Id = @Id;", new { Id = carClassId });
+    }
+}
