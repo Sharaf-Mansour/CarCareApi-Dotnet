a5fd50bd7211cf3ffdcd7f5a35a5a9791a79748d
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-03 19:08:57 +0200

    BROKER:Create Gas

---
+++ CarCareAPI/Brokers/Storages/IStorageBroker.Gas.cs
@@ -0,0 +1,10 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Brokers.Storages;
+public partial interface IStorageBroker
+{
+    ValueTask InsertGasAsync(Gas gas);
+    IQueryable<Gas> SelectAllGasesAsync();
+    ValueTask<Gas> SelectGasByIdAsync(string gasId);
+    ValueTask UpdateGasAsync(Gas gas);
+    ValueTask DeleteGasAsync(string gasId);
+}
