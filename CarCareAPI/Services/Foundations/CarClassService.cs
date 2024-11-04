5d5cbd65d44abde3d64408695de3c451089f6a59
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-04 23:06:27 +0200

    Service:CarClass service

---
+++ CarCareAPI/Services/Foundations/CarClassService.cs
@@ -0,0 +1,16 @@
+﻿using CarCareAPI.Models;
+using CarCareAPI.Brokers.Storages;
+namespace CarCareAPI.Services.Foundations;
+public class CarClassService(IStorageBroker storageBroker) : ICarClassService
+{
+    public async ValueTask AddCarClassAsync(CarClass carClass)
+        => await storageBroker.InsertCarClassAsync(carClass);
+    public async ValueTask<List<CarClass>> RetrieveAllCarClassesAsync()
+        => await storageBroker.SelectAllCarClassesAsync();
+    public async ValueTask<CarClass> RetrieveCarClassByIdAsync(string carClassId)
+        => await storageBroker.SelectCarClassByIdAsync(carClassId);
+    public async ValueTask ModifyCarClassAsync(CarClass carClass)
+        => await storageBroker.UpdateCarClassAsync(carClass);
+    public async ValueTask RemoveCarClassByIdAsync(string carClassId)
+        => await storageBroker.DeleteCarClassAsync(carClassId);
+}
