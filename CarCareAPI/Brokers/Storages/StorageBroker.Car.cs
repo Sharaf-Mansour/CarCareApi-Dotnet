4f949b80720dd8bd1871c17d843cfe77d1da803b
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2024-11-04 20:17:20 +0200

    Update StorageBroker.Car.cs

--- CarCareAPI/Brokers/Storages/StorageBroker.Car.cs
+++ CarCareAPI/Brokers/Storages/StorageBroker.Car.cs
@@ -6,17 +6,17 @@ public partial class StorageBroker : IStorageBroker
     public async ValueTask InsertCarAsync(Car car)
     {
         using var connection = CreateConnection();
-        await connection.ExecuteAsync("INSERT INTO Cars (Id, Make, Model, Year, VIN) VALUES (@Id, @Make, @Model, @Year, @VIN);", car);
+        await connection.ExecuteAsync("INSERT INTO Car (Id, Make, Model, Year, VIN) VALUES (@Id, @Make, @Model, @Year, @VIN);", car);
     }
     public async ValueTask<List<Car>> SelectAllCarsAsync()
     {
         using var connection = CreateConnection();
-        return (await connection.QueryAsync<Car>("SELECT * FROM Cars;")).ToList();
+        return (await connection.QueryAsync<Car>("SELECT * FROM Car;")).AsList();
     }
     public async ValueTask<Car> SelectCarByIdAsync(string carId)
     {
         using var connection = CreateConnection();
-        return await connection.QueryFirstOrDefaultAsync<Car>("SELECT * FROM Cars WHERE Id = @Id;", new { Id = carId });
+        return await connection.QueryFirstOrDefaultAsync<Car>("SELECT * FROM Cars WHERE Id = @carId;", new {carId} );
     }
     public async ValueTask UpdateCarAsync(Car car)
     {
@@ -26,6 +26,6 @@ public partial class StorageBroker : IStorageBroker
     public async ValueTask DeleteCarAsync(string carId)
     {
         using var connection = CreateConnection();
-        await connection.ExecuteAsync("DELETE FROM Cars WHERE Id = @Id;", new { Id = carId });
+        await connection.ExecuteAsync("DELETE FROM Cars WHERE Id = @carId;", new { carId });
     }
 }
