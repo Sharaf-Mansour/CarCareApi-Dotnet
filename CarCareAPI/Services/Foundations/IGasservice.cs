23398c75cdd459f722915568c145fef1c83c61f5
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-04 23:11:05 +0200

    Service:Gas Service

---
+++ CarCareAPI/Services/Foundations/IGasservice.cs
@@ -0,0 +1,10 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Services.Foundations;
+public interface IGasService
+{
+    ValueTask AddGasAsync(Gas gas);
+    ValueTask<List<Gas>> RetrieveAllGasesAsync();
+    ValueTask<Gas> RetrieveGasByIdAsync(string gasId);
+    ValueTask ModifyGasAsync(Gas gas);
+    ValueTask RemoveGasByIdAsync(string gasId);
+}
