416a4632259797c9a49675460707fc2f50d4820d
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-04 23:01:17 +0200

    Service : ArchivedRepair Service

---
+++ CarCareAPI/Services/Foundations/ArchivedRepairService.cs
@@ -0,0 +1,17 @@
+﻿using CarCareAPI.Models;
+using CarCareAPI.Brokers.Storages;
+namespace CarCareAPI.Services.Foundations;
+public class ArchivedRepairService (IStorageBroker storageBroker) : IArchivedRepairService
+{
+    public async ValueTask AddArchivedRepairAsync(ArchivedRepair archivedRepair)
+            => await storageBroker.InsertArchivedRepairAsync(archivedRepair);
+    public async ValueTask<List<ArchivedRepair>> RetrieveAllArchivedRepairsAsync()
+        => await storageBroker.SelectAllArchivedRepairsAsync();
+    public async ValueTask<ArchivedRepair> RetrieveArchivedRepairByIdAsync(string archivedRepairId)
+        => await storageBroker.SelectArchivedRepairByIdAsync(archivedRepairId);
+    public async ValueTask ModifyArchivedRepairAsync(ArchivedRepair archivedRepair)
+        => await storageBroker.UpdateArchivedRepairAsync(archivedRepair);
+    public async ValueTask RemoveArchivedRepairByIdAsync(string archivedRepairId)
+        => await storageBroker.DeleteArchivedRepairAsync(archivedRepairId);
+}
+
---
+++ CarCareAPI/Services/Foundations/IArchivedRepairService.cs
@@ -0,0 +1,10 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Services.Foundations;
+public interface IArchivedRepairService         
+{
+    ValueTask AddArchivedRepairAsync(ArchivedRepair archivedRepair);
+    ValueTask<List<ArchivedRepair>> RetrieveAllArchivedRepairsAsync();
+    ValueTask<ArchivedRepair> RetrieveArchivedRepairByIdAsync(string archivedRepairId);
+    ValueTask ModifyArchivedRepairAsync(ArchivedRepair archivedRepair);
+    ValueTask RemoveArchivedRepairByIdAsync(string archivedRepairId);
+}
