4e324273a36d411b7257f09df26def3644d51201
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-04 23:38:25 +0200

    Broker:RepairType Broker

---
+++ CarCareAPI/Brokers/Storages/IStorageBroker.RepairType.cs
@@ -0,0 +1,10 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Brokers.Storages;
+public partial interface IStorageBroker
+{
+    ValueTask InsertRepairTypeAsync(RepairType repairType);
+    ValueTask<List<RepairType>> SelectAllRepairTypesAsync();
+    ValueTask<RepairType> SelectRepairTypeByIdAsync(string repairTypeId);
+    ValueTask UpdateRepairTypeAsync(RepairType repairType);
+    ValueTask DeleteRepairTypeAsync(string repairTypeId);
+}
