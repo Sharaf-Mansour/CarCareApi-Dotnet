314732b68a4f810006d39d82cce03c7f01122c2f
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-04 23:15:03 +0200

    Service:Profile Service

---
+++ CarCareAPI/Services/Foundations/ProfileService.cs
@@ -0,0 +1,16 @@
+﻿using CarCareAPI.Models;
+using CarCareAPI.Brokers.Storages;
+namespace CarCareAPI.Services.Foundations;
+public class ProfileService(IStorageBroker storageBroker) : IProfileService
+{
+    public async ValueTask AddProfileAsync(Profile profile)
+        => await storageBroker.InsertProfileAsync(profile);
+    public async ValueTask<List<Profile>> RetrieveAllProfilesAsync()
+        => await storageBroker.SelectAllProfilesAsync();
+    public async ValueTask<Profile> RetrieveProfileByIdAsync(string profileId)
+        => await storageBroker.SelectProfileByIdAsync(profileId);
+    public async ValueTask ModifyProfileAsync(Profile profile)
+        => await storageBroker.UpdateProfileAsync(profile);
+    public async ValueTask RemoveProfileByIdAsync(string profileId)
+        => await storageBroker.DeleteProfileAsync(profileId);
+}
