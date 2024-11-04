754f34ec2cd839e9fedce2baded0fca235a6dbe4
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-04 22:22:24 +0200

    Service:Car Service

--- CarCareAPI/Services/Foundations/CarService.cs
+++ CarCareAPI/Services/Foundations/CarService.cs
@@ -1,6 +1,18 @@
-﻿namespace CarCareAPI.Services.Foundations
+﻿using CarCareAPI.Models;
+using CarCareAPI.Brokers.Storages;
+namespace CarCareAPI.Services.Foundations;
+public class CarService (IStorageBroker storageBroker) : ICarService
 {
-    public class CarService
-    {
-    }
+    public async ValueTask AddCarAsync(Car car)
+            => await storageBroker.InsertCarAsync(car);
+    public async ValueTask<List<Car>> RetrieveAllCarsAsync()
+        => await storageBroker.SelectAllCarsAsync();
+    public async ValueTask<Car> RetrieveCarByIdAsync(string carId)
+        => await storageBroker.SelectCarByIdAsync(carId);
+    public async ValueTask ModifyCarAsync(Car car)
+        => await storageBroker.UpdateCarAsync(car);
+    public async ValueTask RemoveCarByIdAsync(string carId)
+        => await storageBroker.DeleteCarAsync(carId);
 }
+
+
---
+++ CarCareAPI/Services/Foundations/ICarService.cs
@@ -0,0 +1,10 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Services.Foundations;
+public interface ICarService
+{
+    ValueTask AddCarAsync(Car car);
+    ValueTask<List<Car>> RetrieveAllCarsAsync();
+    ValueTask<Car> RetrieveCarByIdAsync(string carId);
+    ValueTask ModifyCarAsync(Car car);
+    ValueTask RemoveCarByIdAsync(string carId);
+}
