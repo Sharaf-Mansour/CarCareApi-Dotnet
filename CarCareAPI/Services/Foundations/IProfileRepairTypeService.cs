8899da24d1a2968327e421287a03c60aad567376
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-04 23:22:40 +0200

    Service:ProfileRepairType Service

---
+++ CarCareAPI/Services/Foundations/IProfileRepairTypeService.cs
@@ -0,0 +1,10 @@
+﻿using CarCareAPI.Models;
+namespace CarCareAPI.Services.Foundations;
+public interface IProfileRepairTypeService
+{
+    ValueTask AddProfileRepairTypeAsync(ProfileRepairType profileRepairType);
+    ValueTask<List<ProfileRepairType>> RetrieveAllProfileRepairTypesAsync();
+    ValueTask<ProfileRepairType> RetrieveProfileRepairTypeByIdAsync(string profileRepairTypeId);
+    ValueTask ModifyProfileRepairTypeAsync(ProfileRepairType profileRepairType);
+    ValueTask RemoveProfileRepairTypeByIdAsync(string profileRepairTypeId);
+}
