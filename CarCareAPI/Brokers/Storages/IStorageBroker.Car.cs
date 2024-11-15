382fbed834a4c1a62257665bf45ac54d0492386a
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-11-15 21:47:46 +0200

    Broker:Car

--- CarCareAPI/Brokers/Storages/IStorageBroker.Car.cs
+++ CarCareAPI/Brokers/Storages/IStorageBroker.Car.cs
@@ -4,7 +4,9 @@ public partial interface IStorageBroker
 {
     ValueTask InsertCarAsync(Car car);
     ValueTask<List<Car>> SelectAllCarsAsync();
+    ValueTask<List<(Car car, List<Repair> repairs)>> SelectCarsWithFullDetailsAsync();
     ValueTask<Car> SelectCarByIdAsync(string carId);
     ValueTask UpdateCarAsync(Car car);
     ValueTask DeleteCarAsync(string carId);
+    ValueTask<(Car car, List<Repair> repairs)> SelectCarWithRepairsAsync(string carId);
 }
--- CarCareAPI/Brokers/Storages/StorageBroker.Car.cs
+++ CarCareAPI/Brokers/Storages/StorageBroker.Car.cs
@@ -13,16 +13,43 @@ public partial class StorageBroker : IStorageBroker
         using var connection = CreateConnection();
         return (await connection.QueryAsync<Car>("SELECT * FROM Car;")).AsList();
     }
+
+    public async ValueTask<List<(Car car, List<Repair> repairs)>> SelectCarsWithFullDetailsAsync()
+    {
+        using var connection = CreateConnection();
+        using var multi = await connection.QueryMultipleAsync(@"SELECT * FROM Car;
+        SELECT * FROM Repair;");
+        var cars = multi.Read<Car>().ToList();
+        var repairs = multi.Read<Repair>().ToList();
+        var carRepairList = new List<(Car car, List<Repair> repairs)>();
+        foreach (var car in cars)
+        {
+            var carRepairs = repairs.Where(repair => repair.id == car.id).ToList();
+            carRepairList.Add((car, carRepairs));
+        }
+        return carRepairList;
+    }
+
     public async ValueTask<Car> SelectCarByIdAsync(string carId)
     {
         using var connection = CreateConnection();
         return await connection.QueryFirstOrDefaultAsync<Car>("SELECT * FROM Car WHERE Id = @carId;", new { carId });
     }
+
+    public async ValueTask<(Car car, List<Repair> repairs)> SelectCarWithRepairsAsync(string carId)
+    {
+        using var connection = CreateConnection();
+        using var multi = await connection.QueryMultipleAsync(@"
+        SELECT * FROM Cars WHERE Id = @CarId;
+        SELECT * FROM Repairs WHERE CarId = @CarId;", new { CarId = carId });
+        var car = multi.Read<Car>().FirstOrDefault();
+        var repairs = multi.Read<Repair>().ToList();
+        return (car, repairs);
+    }
     public async ValueTask UpdateCarAsync(Car car)
     {
         using var connection = CreateConnection();
         await connection.ExecuteAsync("UPDATE Car SET Make = @Make, Model = @Model, Year = @Year, ClassId = @ClassId, LicenseExpirationDate = @LicenseExpirationDate, ExaminationDate = @ExaminationDate, NotifyEvery = @NotifyEvery, Km = @Km, ProfileId = @ProfileId WHERE Id = @Id;", car);
-
     }
     public async ValueTask DeleteCarAsync(string carId)
     {
